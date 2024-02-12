c60092b8d883a838835a62b52078125e8394a10e
vishesh143 <visheshkuhar143@gmail.com>
2024-02-12 22:27:43 +0530

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+
+      static int num=10;
+
+    public static void main(String[] args) {
+        num=5;
+        System.out.println("Num:"+num);
+        Main2.fun3();
+    }
+
+}
---
+++ src/main/java/org/example/Main2.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main2 {
+
+   static void fun3(){
+        System.out.println("Hi this is fun 3");
+    }
+}
